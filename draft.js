0cc421cbca06831895132cbc3ae2b42f401871a0
Yun Zhou <zhouyun@ecs.vuw.ac.nz>
2021-09-16 01:15:41 +0800

    no error after run it, so it means the firebase sdk package is installed successfully and the code which is require() is quoted successfully

---
+++ draft.js
@@ -0,0 +1,9 @@
+/**
+ * this file is a draft for testing whether firebase-admin (i.e. firebase SDK) is working or not.
+ * 
+ * Of course, we use node js to do this, which means, we run: `node draft.js`
+ * 
+ * If no error after run it, then it means the firebase sdk package is installed successfully and the code which is require('firebase-admin') is quoted successfully
+ */
+
+var admin = require('firebase-admin');
