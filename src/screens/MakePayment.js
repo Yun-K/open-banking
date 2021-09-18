83bacd144086fb181fbd0814e8f3cf676e5b8f03
Ze Chen <chenze@ecs.vuw.ac.nz>
2021-09-18 20:54:06 +0800

    try to switch interface

--- src/screens/MakePayment.js
+++ src/screens/MakePayment.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import MakePayment from './MakePayment';
-import Managepayment from'./Managepayment'
-import {StackNavigator}from 'react-navigation'
 import{
     View,
     Text,
@@ -12,11 +9,12 @@ import{
     TouchableOpacity,
     Image
 }from 'react-native'
+
 export default class Payment extends React.Component{
     render(){
         return(
             <View style = {styles.container}>
-               <TouchableOpacity onPress={() =>this.props.navigation.navigate('MakePayment')}>
+               <TouchableOpacity onPress={() => Alert.alert('go to make payment')}>
                <Image source={require("../../res/images/Make-payment.png")}
                style={{
                 width: 400,
@@ -33,41 +31,22 @@ export default class Payment extends React.Component{
             </View>          
             )
     }
-    
      
 }
 
-const Stack = StackNavigator(
-{
-  MakePayment:{
-    screen:MakePayment,
-  },
-  Managepayment:{
-    screen:Managepayment,
-  },
-}
-
-);
-
-
-
-
-
 const styles = StyleSheet.create({
-  container:{
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  Button:{
-    backgroundColor: '#859a9b',
-    borderRadius: 20,
-    padding: 10,
-    marginBottom: 20,
-    shadowColor: '#303838',
-    shadowOffset: { width: 0, height: 5 },
-    shadowRadius: 10,
-    shadowOpacity: 0.35,
-  }
-})
-
-
+    container:{
+      flex: 1,
+      backgroundColor: '#fff',
+    },
+    Button:{
+      backgroundColor: '#859a9b',
+      borderRadius: 20,
+      padding: 10,
+      marginBottom: 20,
+      shadowColor: '#303838',
+      shadowOffset: { width: 0, height: 5 },
+      shadowRadius: 10,
+      shadowOpacity: 0.35,
+    }
+  })
