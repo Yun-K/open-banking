71d6ef299d207fc3ce914982ddf6b14f27486932
Ze Chen <chenze@ecs.vuw.ac.nz>
2021-09-18 10:49:33 +0800

    test

---
+++ src/screens/Payment.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import{
+    View,
+    Text,
+    StyleSheet,
+    Button
+}from 'react-native'
+
+export default class Payment extends React.Component{
+    render(){
+        return(
+            <View style = {styles.container}>
+            </View>          
+            )
+    }
+     
+}
+
+const styles = StyleSheet.create({
+  container:{
+     flex : 1,
+     backgroundColor: '#eee',
+  },
+  title:{
+    textAlign: 'center',
+    marginVertical: 8,
+  }
+})
+
+
