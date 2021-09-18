3dc4b9a40ffcc18e71994336d7f88a64df8f00b5
Wang Conglang <wangcong@ecs.vuw.ac.nz>
2021-09-18 13:51:38 +1200

    Horizontal sliding bar

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -11,6 +11,7 @@ import {
     Text,
     useColorScheme,
     View,
+    Image
 } from 'react-native';
 
 import {
@@ -21,8 +22,63 @@ import {
     ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+
+
 const HomeScreen = ({ navigation }) => {
-    return <Text>This is HomeScreen</Text>;
+    return (<View><ScrollView scrollEventThrottle={16}>
+                                <View style = {{ flex:1, backgroundColor:'white',paddingTop: 20 ,height:250}}>
+                                    <Text>
+                                        Banks
+                                    </Text>
+                                    <View style={{ height:130,marginTop:20 }}>
+                                        <ScrollView horizontal={true} showsHorizontalScrollIndicator={true}>
+                                            <View style={{height:130,width:130,marginLeft:20,borderWidth:0.5,borderColor:'#dddddd'}}>
+                                                 <View style={{flex:2,paddingLeft:10,paddingTop:10}}>
+                                                                   <Image source={require("../screens/HomeScreenImage/ANZ.jpg")} style={{flex:1,width:null,height:null,resizeMode:'cover'}}/>
+                                                 </View>
+                                                 <View style={{flex:1,paddingLeft:10,paddingTop:10}}>
+                                                                   <Text>
+                                                                       20-202000-00
+                                                                   </Text>
+                                                                   <Text>
+                                                                        20-202000-01
+                                                                   </Text>
+                                                 </View>
+                                            </View>
+
+                                            <View style={{height:130,width:130,marginLeft:20,borderWidth:0.5,borderColor:'#dddddd'}}>
+                                                 <View style={{flex:2,paddingLeft:10,paddingTop:10}}>
+                                                                   <Image source={require("../screens/HomeScreenImage/bnz.png")} style={{flex:1,width:null,height:null,resizeMode:'cover'}}/>
+                                                 </View>
+                                                 <View style={{flex:1,paddingLeft:10,paddingTop:10}}>
+                                                                   <Text>
+                                                                       20-202000-00
+                                                                   </Text>
+                                                                   <Text>
+                                                                        20-202000-01
+                                                                   </Text>
+                                                 </View>
+                                            </View>
+
+                                            <View style={{height:130,width:130,marginLeft:20,borderWidth:0.5,borderColor:'#dddddd'}}>
+                                                 <View style={{flex:2,paddingLeft:10,paddingTop:10}}>
+                                                                   <Image source={require("../screens/HomeScreenImage/west.jpg")} style={{flex:1,width:null,height:null,resizeMode:'cover'}}/>
+                                                 </View>
+                                                 <View style={{flex:1,paddingLeft:10,paddingTop:10}}>
+                                                                   <Text>
+                                                                       20-202000-00
+                                                                   </Text>
+                                                                   <Text>
+                                                                        20-202000-01
+                                                                   </Text>
+                                                 </View>
+                                            </View>
+
+                                        </ScrollView>
+                                    </View>
+                                </View>
+                             </ScrollView>
+    </View>);
 };
 
 export default HomeScreen
