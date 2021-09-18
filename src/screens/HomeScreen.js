081cd79b61cf2ecae1a60b42605a76c94fdec76f
Wang Conglang <wangcong@ecs.vuw.ac.nz>
2021-09-18 15:17:39 +1200

    Horizontal sliding bar plus

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -27,17 +27,17 @@ import {
 const HomeScreen = ({ navigation }) => {
     return (<View><ScrollView scrollEventThrottle={16}>
                                 <View style = {{ flex:1, backgroundColor:'white',paddingTop: 20 ,height:250}}>
-                                    <Text>
+                                    <Text style={{ fontSize:24 ,fontWeight:'700',paddingHorizontal:20 }}>
                                         Banks
                                     </Text>
                                     <View style={{ height:130,marginTop:20 }}>
                                         <ScrollView horizontal={true} showsHorizontalScrollIndicator={true}>
                                             <View style={{height:130,width:130,marginLeft:20,borderWidth:0.5,borderColor:'#dddddd'}}>
-                                                 <View style={{flex:2,paddingLeft:10,paddingTop:10}}>
+                                                 <View style={{flex:2,paddingLeft:8,paddingTop:10,width:120}}>
                                                                    <Image source={require("../screens/HomeScreenImage/ANZ.jpg")} style={{flex:1,width:null,height:null,resizeMode:'cover'}}/>
                                                  </View>
-                                                 <View style={{flex:1,paddingLeft:10,paddingTop:10}}>
-                                                                   <Text>
+                                                 <View style={{flex:1,paddingLeft:20,paddingTop:10}}>
+                                                                   <Text >
                                                                        20-202000-00
                                                                    </Text>
                                                                    <Text>
@@ -47,10 +47,10 @@ const HomeScreen = ({ navigation }) => {
                                             </View>
 
                                             <View style={{height:130,width:130,marginLeft:20,borderWidth:0.5,borderColor:'#dddddd'}}>
-                                                 <View style={{flex:2,paddingLeft:10,paddingTop:10}}>
+                                                 <View style={{flex:2,paddingLeft:8,paddingTop:10,width:120}}>
                                                                    <Image source={require("../screens/HomeScreenImage/bnz.png")} style={{flex:1,width:null,height:null,resizeMode:'cover'}}/>
                                                  </View>
-                                                 <View style={{flex:1,paddingLeft:10,paddingTop:10}}>
+                                                 <View style={{flex:1,paddingLeft:20,paddingTop:10}}>
                                                                    <Text>
                                                                        20-202000-00
                                                                    </Text>
@@ -61,15 +61,26 @@ const HomeScreen = ({ navigation }) => {
                                             </View>
 
                                             <View style={{height:130,width:130,marginLeft:20,borderWidth:0.5,borderColor:'#dddddd'}}>
-                                                 <View style={{flex:2,paddingLeft:10,paddingTop:10}}>
+                                                 <View style={{flex:2,paddingLeft:8,paddingTop:10,width:120}}>
                                                                    <Image source={require("../screens/HomeScreenImage/west.jpg")} style={{flex:1,width:null,height:null,resizeMode:'cover'}}/>
                                                  </View>
-                                                 <View style={{flex:1,paddingLeft:10,paddingTop:10}}>
+                                                 <View style={{flex:1,paddingLeft:20,paddingTop:10}}>
                                                                    <Text>
                                                                        20-202000-00
                                                                    </Text>
                                                                    <Text>
                                                                         20-202000-01
+                                                                   </Text>
+                                                 </View>
+                                            </View>
+
+                                            <View style={{height:130,width:130,marginLeft:20,borderWidth:0.5,borderColor:'#dddddd'}}>
+                                                 <View style={{flex:2,paddingLeft:8,paddingTop:10,width:120}}>
+                                                                   <Image source={require("../screens/HomeScreenImage/plus.jpg")} style={{flex:1,width:null,height:null,resizeMode:'cover'}}/>
+                                                 </View>
+                                                 <View style={{flex:1,paddingLeft:35,paddingTop:10}}>
+                                                                   <Text>
+                                                                       add here
                                                                    </Text>
                                                  </View>
                                             </View>
